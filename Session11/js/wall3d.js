72fb2ed9e5190d2458ef43c4612615cc830c22e1
Sambalim <limsungho07@hanmail.net>
2020-08-22 23:50:40 +0900

    전진! 3D 스크롤 9

--- Session11/js/wall3d.js
+++ Session11/js/wall3d.js
@@ -1,11 +1,17 @@
 (function() {
 
   const houseElem = document.querySelector('.house');
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue;
+
+  const resizeHandler = function() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener('scroll', function() {
     const zMove = pageYOffset / maxScrollValue * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`;
   });
 
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
 })();
